0d8c50ea9be29169c12cd246166bc8d7cc404bae
BryB <bryanbrady6@gmail.com>
2019-01-17 21:06:10 -0800

    adding tabs

--- src/tabs.js
+++ src/tabs.js
@@ -3,7 +3,7 @@ import {renderElement} from './helpers.js';
 function initTabs() {
   let addTab = `<div id="tabs"><button id='tab'>+</button></div>`;
   let defaultTab = `<div id="tabs"><button id='tab'>List</button></div>`;
-  let tabs = addTab + defaultTab
+  let tabs = addTab + defaultTab;
   renderElement('#body', tabs);
 }
 
